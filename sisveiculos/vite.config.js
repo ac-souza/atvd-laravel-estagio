fa1b5935eff084f4d57952e76d3be01a3f93498d
augustocesarrochasouza@gmail.com <augusto.souza@detran.pa.gov.br>
2026-07-23 13:33:01 -0300

    Removendo JS do Vite e build do package.json para ignorar o vite

--- sisveiculos/vite.config.js
+++ sisveiculos/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: [],
             refresh: true,
         }),
         tailwindcss(),
